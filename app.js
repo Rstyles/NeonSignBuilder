07693a23068d6f43d958a626616ef865b8845ff2
Ryan Styles <ryan.c.styles@gmail.com>
2022-01-10 14:25:40 -0600

    Added source files

--- app.js
+++ app.js
@@ -2,25 +2,15 @@ var canvasContext = document.getElementById('neonSignCanvas').getContext('2d');
 var imageElement = document.getElementById('neonSignImage');
 var canvasText = document.getElementById('neonSignText');
 
-// document.getElementById('neonSignText').addEventListener('keyup', function(){
-//     canvasContext.canvas.width = canvasContext.measureText(this.value).width;
-//     canvasContext.font = '45px sans-serif';
-//     canvasContext.fillStyle = document.getElementById('neonSignColor').value;;
-//     canvasContext.fillText(this.value, 0, 40);
-//     imageElement.src = canvasContext.canvas.toDataURL();
-//     console.log(imageElement.src);
-//     checkIfEmpty();
-// }, false);
-
 function drawImage() {
-    canvasContext.clearRect(0, 0, 300, 300);
-    canvasContext.width = canvasContext.measureText(canvasText.value).width;
+    canvasContext.clearRect(0, 0, 500, 500);
     canvasContext.font = '60px sans-serif';
+    canvasContext.textAlign = 'center';
     canvasContext.fillStyle = document.getElementById('neonSignColor').value;
-    canvasContext.fillText(canvasText.value, 0, 50);
+    canvasContext.fillText(canvasText.value, 300, 50);
     imageElement.src = canvasContext.canvas.toDataURL();
     console.log(imageElement.src);
-    checkIfEmpty();
+    //checkIfEmpty();
 }
 
 function checkIfEmpty() {
