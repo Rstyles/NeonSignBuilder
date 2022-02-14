8c1e145aa352b3dbfcf1c0854498f3acebe9748d
Rstyles <ryan.c.styles@gmail.com>
2022-02-14 11:56:24 -0600

    Added counter to simulate pricing

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
-var canvasContext = document.getElementById('neonSignCanvas').getContext('2d');
-var imageElement = document.getElementById('neonSignImage');
-var canvasText = document.getElementById('neonSignText');
+function submitNeonSign() {
+    var canvas = document.getElementById('neonSignCanvas');
+    var context = canvas.getContext('2d');
+    var image = document.getElementById('neonSignImage');
+    var text = document.getElementById('neonSignText');
 
-function drawImage() {
-    canvasContext.clearRect(0, 0, 500, 500);
-    canvasContext.font = '60px sans-serif';
+    var price = document.getElementById('neonSignPrice');
+
+    var font = '60px ' + document.querySelector('input[name="signFont"]:checked').value;
+    var color = document.querySelector('input[name="SignColor"]:checked').value;
+
+    drawImage(canvas, context, image, text, font, color);
+
+    numberWhitePixels = countPixels(canvas, context);
+    console.log(numberWhitePixels);
+    price.innerHTML = '$' + parseFloat(numberWhitePixels/10).toFixed(2);
+}
+
+function drawImage(neonSignCanvas, canvasContext, imageElement, canvasText, canvasfont, canvasColor) {
+    canvasContext.fillStyle = 'white';
+    canvasContext.font = canvasfont;
+    canvasContext.shadowColor = canvasColor;
+    canvasContext.shadowBlur = 20;
     canvasContext.textAlign = 'center';
-    canvasContext.fillStyle = document.getElementById('neonSignColor').value;
-    canvasContext.fillText(canvasText.value, 300, 50);
+
+    canvasContext.clearRect(0, 0, 900, 600);
+    
+    var textMeasurement = canvasContext.measureText(canvasText.value);
+    //console.log(textMeasurement.width);
+    
+    canvasContext.fillText(canvasText.value, neonSignCanvas.width/2, 50, textMeasurement.width);
     imageElement.src = canvasContext.canvas.toDataURL();
-    console.log(imageElement.src);
-    //checkIfEmpty();
 }
 
-function checkIfEmpty() {
-    if (imageElement.src == ',' || imageElement.src == null) {
-        imageElement.style.display = 'none';
-    }
-}
+function countPixels(canvas, context) {
+	var imageData =  context.getImageData(0, 0, canvas.width, canvas.height);
+	var rgba = [];
+    var counter = 0;
+
+	for(var index = 0; index < imageData.data.length; index += 4) {
+		rgba = [imageData.data[index], 
+			    imageData.data[index + 1], 
+		    	imageData.data[index + 2],
+                (imageData.data[index + 3] / 255)];
+
+        if (rgba[0] == 255 && rgba[1] == 255 && rgba[2] == 255 && rgba[3] == 1) {
+            //console.log(rgba);
+            counter++;
+        }
+	}
+    return counter;
+} 
