0c63b7412cfcba6f6bbe1ed75e0aff68157b5272
Rstyles <ryan.c.styles@gmail.com>
2022-03-10 14:12:03 -0600

    Converted to WP Plugin

---
+++ NeonSignBuilder.php
@@ -0,0 +1,122 @@
+<?php
+/**
+Plugin Name: Neon Sign Custom Builder
+Plugin URI: https://nozakconsulting.com
+Description: Easily embed a Neon Sign builder into a page using a shortcode
+Version: 1.3.0
+Author: Ryan Styles - Nozak Consulting
+Author URI: https://nozakconsulting.com
+License: GPL2
+*/
+
+if ( !defined( 'ABSPATH' ) ) exit;
+
+class NZ_NeonSignBuilder_Embed {
+
+	public function __construct() {
+		add_shortcode( 'neon_sign_builder', array($this, 'nsb_shortcode' ) );
+	}
+
+	/**
+	 * Shortcode [neon_sign_builder]
+	 */
+
+	public function nsb_shortcode( $atts, $content ) {
+
+
+		return "
+		<link rel=\"stylesheet\" type=\"text/css\" href='".plugins_url( 'dist/style.css', __FILE__ )."' />
+		<div class=\"canvas_wrapper\">
+		<div class=\"canvas_left\">
+		  <canvas id=\"neonSignCanvas\"></canvas>
+		  <span class=\"neonSignHelper\"></span>
+		  <img id=\"neonSignImage\" />
+		</div>
+		<br />
+	
+		<div class=\"canvas_right\">
+		  <form action=\"#n\" name=\"colorPicker\" class=\"neonSignEditorForm\" onchange=\"submitNeonSign()\">
+			<label for=\"neonSignText\">Text:</label>
+			<textarea name=\"neonSignText\" id=\"neonSignText\" onkeyup=\"submitNeonSign()\"></textarea>
+	
+			<h3 class=\"neonSignHeading\">Color:</h3>
+			<div class=\"colorPickerRadioGroup\">
+			  <input type=\"radio\" name=\"SignColor\" id=\"ColorRed\" value=\"red\" checked=\"true\" />
+			  <input type=\"radio\" name=\"SignColor\" id=\"ColorGreen\" value=\"green\" />
+			  <input type=\"radio\" name=\"SignColor\" id=\"ColorBlue\" value=\"blue\" />
+			  <input type=\"radio\" name=\"SignColor\" id=\"ColorOrange\" value=\"orange\" />
+			  <input type=\"radio\" name=\"SignColor\" id=\"ColorPink\" value=\"pink\" />
+			  <input type=\"radio\" name=\"SignColor\" id=\"ColorWhite\" value=\"#eee\" />
+			  <input type=\"radio\" name=\"SignColor\" id=\"ColorYellow\" value=\"yellow\" />
+			  <input type=\"radio\" name=\"SignColor\" id=\"ColorIceBlue\" value=\"#24B7DE\" />
+			</div>
+			<hr>
+			<h3 class=\"neonSignHeading\">Font:</h3>
+			<div id=\"fontPickerRadioGroup\" class=\"fontGroup\">
+			  <input type=\"radio\" name=\"signFont\" id=\"fontComicSans\" value=\"Comic Sans MS, Comic Sans, cursive\"
+				checked=\"true\" />
+			  <label for=\"fontComicSans\" style=\"font-family: Comic Sans MS, Comic Sans, cursive\">Comic Sans</label>
+	
+			  <input type=\"radio\" name=\"signFont\" id=\"fontTrattatello\" value=\"Trattatello, fantasy\" />
+			  <label for=\"fontTrattatello\" style=\"font-family: Trattatello, fantasy\">Trattatello</label>
+	
+			  <input type=\"radio\" name=\"signFont\" id=\"fontAndaleMono\" value=\"Andale Mono, monospace\" />
+			  <label for=\"fontAndaleMono\" style=\"font-family: Andale Mono, monospace\">Andale Mono</label>
+	
+			  <input type=\"radio\" name=\"signFont\" id=\"fontHelvetica\" value=\"Helvetica, sans-serif\" />
+			  <label for=\"fontHelvetica\" style=\"font-family: Helvetica, sans-serif\">Helvetica</label>
+			</div>
+			<hr>
+	
+			<h3 class=\"neonSignHeading\">Size:</h3>
+			<div id=\"fontSizeSliderWrapper\" class=\"fontGroup\">
+			  <input type=\"range\" name=\"fontSizeSlider\" id=\"fontSizeSlider\" class=\"fontSizeSlider\" min=\"50\" max=\"75\" onmousemove=\"submitNeonSign()\">
+			</div>
+			<hr>
+	
+			<h3 class=\"neonSignHeading\">Support:</h3>
+			<div class=\"supportOptions\">
+			  <input type=\"radio\" name=\"neonSignSupportType\" id=\"cutToShape\" value=\"Cut to Shape\" checked=\"true\">
+			  <label for=\"cutToShape\">Cut to Shape -
+				<span class=\"flavor-text\">Acrylic display that follows the shape of your sign.</span>
+			  </label>
+			</div>
+	
+			<div class=\"supportOptions\">
+			  <input type=\"radio\" name=\"neonSignSupportType\" id=\"hollowOut\" value=\"Hollow Out\">
+			  <label for=\"hollowOut\">Hollow Out -
+				<span class=\"flavor-text\">The most descreet transparent backing.</span>
+			  </label>
+			</div>
+	
+			<div class=\"supportOptions\">
+			  <input type=\"radio\" name=\"neonSignSupportType\" id=\"fullBoard\" value=\"Full Board\">
+			  <label for=\"fullBoard\">Full Board -
+				<span class=\"flavor-text\">Acrylic display in shape of a square around your sign.</span>
+			  </label>
+			</div>
+	
+			<div class=\"supportOptions\">
+			  <input type=\"radio\" name=\"neonSignSupportType\" id=\"stand\" value=\"Stand\">
+			  <label for=\"stand\">Stand -
+				<span class=\"flavor-text\">Make your sign upright on the floor.</span>
+			  </label>
+			</div>
+		  </form>
+	
+		  <button onclick=\"submitNeonSign()\" id=\"neonSignSubmit\">Submit</button>
+	
+		  <div class=\"neonSignPriceWrapper\">
+			<p id=\"neonSignPrice\"></p>
+		  </div>
+		</div>
+	  </div>
+	  <script type='text/javascript' src='".plugins_url('dist/NeonSignCanvas.js', __FILE__)."'></script>
+	  <script type='text/javascript' src='".plugins_url('dist/calculatePrice.js', __FILE__)."'></script>
+	  <script type='text/javascript' src='".plugins_url('dist/drawImage.js', __FILE__)."'></script>
+	  <script type='text/javascript' src='".plugins_url('dist/dragItem.js', __FILE__)."'></script>";
+	}
+
+}
+
+new NZ_NeonSignBuilder_Embed();
--- dist/NeonSignCanvas.js
+++ dist/NeonSignCanvas.js
@@ -1,63 +1,16 @@
 "use strict";
-const canvas = document.getElementById("neonSignCanvas");
-const context = canvas.getContext("2d");
+var canvas = document.getElementById("neonSignCanvas");
+var context = canvas.getContext("2d");
 var text = document.getElementById("neonSignText");
-const image = document.getElementById("neonSignImage");
+var image = document.getElementById("neonSignImage");
 function submitNeonSign() {
     var price = document.getElementById("neonSignPrice");
-    //var basePrice = 0;
     var pricePerInch = 1.27;
     var fontSize = document.querySelector('input[name="fontSizeSlider"]').value;
-    var font = fontSize +
-        "px " +
-        document.querySelector('input[name="signFont"]:checked').value;
+    var font = fontSize + "px " + document.querySelector('input[name="signFont"]:checked').value;
     var color = document.querySelector('input[name="SignColor"]:checked').value;
     drawImage(canvas, context, image, text, font, color);
     var numberWhitePixels = countPixels(canvas, context);
     price.innerHTML = "$" + calculatePrice(pricePerInch, numberWhitePixels);
     console.log(numberWhitePixels);
-}
-function drawImage(neonSignCanvas, canvasContext, imageElement, canvasText, canvasfont, canvasColor, x = neonSignCanvas.width / 2, y = 70) {
-    canvasContext.clearRect(0, 0, neonSignCanvas.width, neonSignCanvas.height);
-    canvasContext.textBaseline = "ideographic";
-    var textMeasurement = canvasContext.measureText(canvasText.value);
-    neonSignCanvas.width = textMeasurement.width + 150;
-    neonSignCanvas.height = 225;
-    canvasContext.fillStyle = "white";
-    canvasContext.font = canvasfont;
-    canvasContext.shadowColor = canvasColor;
-    canvasContext.shadowBlur = 20;
-    canvasContext.textAlign = "center";
-    drawLines(canvasContext, canvasText.value, x, y, 70);
-    imageElement.src = canvasContext.canvas.toDataURL();
-}
-function drawLines(context, text, x, y, lineHeight) {
-    var lines = text.split("\n");
-    if (lines.length > 3) {
-        lines.length = 3;
-    }
-    for (var i = 0; i < lines.length; i++) {
-        context.fillText(lines[i], x, y + i * lineHeight);
-    }
-}
-function countPixels(canvas, context) {
-    var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-    var rgba = [];
-    var counter = 0;
-    for (var index = 0; index < imageData.data.length; index += 4) {
-        rgba = [
-            imageData.data[index],
-            imageData.data[index + 1],
-            imageData.data[index + 2],
-            imageData.data[index + 3] / 255,
-        ];
-        if (rgba[0] == 255 && rgba[1] == 255 && rgba[2] == 255 && rgba[3] == 1) {
-            counter++;
-        }
-    }
-    return counter;
-}
-function calculatePrice(pricePerInch, pixelCount) {
-    var numInches = (pixelCount * 13) / 136;
-    return parseFloat(numInches * pricePerInch).toFixed(2);
 }
