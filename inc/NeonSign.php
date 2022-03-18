748953551de2ee350f9c88c982bc6965cef6310f
Ryan Styles <ryan.c.styles@gmail.com>
2022-03-18 16:44:24 -0500

    Fixed some things, broke some things

--- inc/NeonSign.php
+++ inc/NeonSign.php
@@ -3,34 +3,62 @@
  * @package NeonSignBuilder-NozakConsulting
  */
 
-require_once('.\Api\WoocommerceService.php');
-
-use Inc\Api\WoocommerceService;
-
-$err = [];
-
 $parse_uri = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
 require_once( $parse_uri[0] . 'wp-load.php' );
 
-$service = new WoocommerceService();
+use Automattic\WooCommerce\Client;
+use Inc\Api\WoocommerceService;
 
-$service->setConsumer_key(get_option('nz_woocommerce_consumer_key'));
+$err = [];
+$consumer_key = "";
+$consumer_secret = "";
+$store_url = get_site_url();
 
-$woocommerce = $service->init();
 
-$sign = $service->createProduct(
-    'Custom Sign',
-    'Simple product',
-    '$150',
-    'Dimensions:  /n
-        Color:'. $_POST['SignColor']. '/n',
-    '1',
-    '',
-    $woocommerce
-);
 
-foreach($err as $error){
-    echo $error . ' ';
+
+if ( null !== get_option( 'nz_woocommerce_consumer_key' ) ) {
+    $consumer_key = get_option( 'nz_woocommerce_consumer_key' );
 }
 
-echo $_POST['SignColor'];
+if ( null !== get_option( 'nz_woocommerce_consumer_secret' ) ) {
+    $consumer_secret = get_option( 'nz_woocommerce_consumer_secret' );
+}
+
+
+$woocommerce = new Client(
+    $store_url . 'wp-json/',
+    $consumer_key,
+    $consumer_secret,
+    [
+        'wp_api' => true,
+        'version' => 'wc/v3',
+        'verify_ssl' => false
+    ]
+);
+
+$data = [
+    'name' => 'Premium Quality',
+    'type' => 'simple',
+    'regular_price' => '21.99',
+    'description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.',
+    'short_description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+    'categories' => [
+        [
+            'id' => 9
+        ],
+        [
+            'id' => 14
+        ]
+    ],
+    'images' => [
+        [
+            'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg'
+        ],
+        [
+            'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg'
+        ]
+    ]
+];
+
+print_r($woocommerce->post('products', $data));
